c8d422a142d3a868f36fef97fc2110e90e88dc01
David <davidimbachi2111@hotmail.com>
2025-09-03 16:22:09 -0500

    backen

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,6 +7,6 @@ urlpatterns= [
     path("", views.login, name="login"),
     path("registro", Registro.as_view(), name="register"),
     path("recuperar_password/<str:email>", views.recovery_password, name="recovery_password"),
-    path("instructores/", views.instructores, name="instructores"),
+    path("instructores/", views.list_user_by_area, name="instructores"),
 ]
 
