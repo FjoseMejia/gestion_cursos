ed83cbaf9f006a0b62bcc6e7b39569cd729ecf56
FjoseMejia <153628140+FjoseMejia@users.noreply.github.com>
2025-09-06 13:34:54 -0500

    borrando comentarios y lineas repetidas de códigos.

--- config/urls.py
+++ config/urls.py
@@ -21,10 +21,8 @@ from ofertas import views as ofertas_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('', include('usuarios.urls_reset')),     # Este NO tiene app_name
+    path('', include('usuarios.urls_reset')),
     path('usuarios/', include('usuarios.urls')),
     path('ofertas/', include('ofertas.urls')),
-    #path('inscripciones', include('inscripciones.urls')),
-    path('inscripciones/', include('inscripciones.urls')),  # namespace: inscripciones
-    
+    path('inscripciones/', include('inscripciones.urls')),
 ]
