12419dd1f838226221d5fda70d0b2d9d275faa53
David <davidimbachi2111@hotmail.com>
2025-09-10 18:52:36 -0500

    solucion de errores

--- ofertas/static/js/oferta.js
+++ ofertas/static/js/oferta.js
@@ -186,3 +186,16 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
  }
+
+function cargarArchivo(tipo) {
+  const div = document.getElementById(tipo);
+  div.classList.add("cargado");
+}
+
+function enviarLink() {
+  alert("📎 Se envió un link de descarga.");
+}
+
+function enviarArchivos() {
+  alert("✅ Archivos enviados correctamente.");
+}
