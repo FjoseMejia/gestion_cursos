1c54f3b4af3867483c10a29c87e8bc29d3cb725f
Alejandrol0 <alejandrovalenzuela1416@gmail.com>
2025-09-10 08:18:17 -0500

    Update urls.py

--- inscripciones/urls.py
+++ inscripciones/urls.py
@@ -9,11 +9,8 @@ app_name = 'inscripciones'
 urlpatterns = [
     # Todos los patrones de URL deben estar en una sola lista
     path('', views.inscripcion_formulario, name='index'),
-    path('admin/', admin.site.urls),
     path('exportar-excel/', views.exportar_a_excel, name='exportar_excel') ,
     path('exportar-personales/', views.exportar_datos_personales, name='exportar_personales'),
-    path('incripciones/', views.inscripcion_formulario, name='inscripciones'),
-    path('', views.index, name='index'),
     path('detalle/<int:pk>/', views.detalle_inscripcion, name='detalle_inscripcion'),
 
     # La URL para exportar el archivo de Excel
