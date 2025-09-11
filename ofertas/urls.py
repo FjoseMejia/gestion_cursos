9dd7c382d92a343f8dd6403ae1988ba4fb03555e
David <davidimbachi2111@hotmail.com>
2025-09-11 09:17:55 -0500

    WIP: guardando trabajo antes de cambiar de rama

--- ofertas/urls.py
+++ ofertas/urls.py
@@ -8,6 +8,7 @@ urlpatterns= [
     path("solicitudes/", views.solicitudes, name="solicitudes"),
     path('solicitud/', views.index, name='solicitud'),
     path("reportes/", views.reportes, name='reportes'),
+    path("reportes/exportar/", views.exportar_a_excel, name="exportar_a_excel"),
     path("reportes/crear/", views.crear_reporte, name='crear_reporte'),
     path('api/programas_sugeridos/', views.programas_sugeridos, name='programas_sugeridos'),
     path('cambiar-estado/<int:oferta_id>/<str:accion>/', views.cambiar_estado, name='cambiar_estado'),
